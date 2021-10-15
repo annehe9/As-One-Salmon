6619b62682b1462689fc22df15da65800f53fccb
miatang13 <miatang13@gmail.com>
2021-10-15 10:30:24 -0400

    Add new control prototype by Paolo

---
+++ As One (new control)/Assets/MovementTwoAxis.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementTwoAxis : MonoBehaviour
+{
+    //since it's 3d I had to freeze the rotation of the capsule on the rigidbody to prevent the capsule from tumbling around
+    public Rigidbody rb;
+    public float moveSpeed = 5f;
+    public bool frozen = false;
+    public float torque = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //cache the rigidbody on this game object
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    //I put the physics update here - fixed framerate = consistent
+    //any physics in the Update() has to be multiplied by Time.deltaTime to be frame independent
+    private void FixedUpdate()
+    {
+        if (!frozen)
+        {
+            float thrust = Input.GetAxis("Vertical");
+
+            float turn = Input.GetAxis("Horizontal");
+            rb.AddTorque(transform.up * torque * turn);
+            rb.AddForce(transform.forward * thrust * moveSpeed);
+            
+        }
+    }
+
+    //this functions can be called by the dialogue manager and block the controls
+    public void Freeze()
+    {
+        frozen = true;
+    }
+
+    public void UnFreeze()
+    {
+        frozen = false;
+    }
+}
---
+++ inky prototypes/sea horse.ink
@@ -0,0 +1,8 @@
+- You go west.
++ You come across a pair of sea horses
+  + + Ask for their names
+    I'm Brad!
+    I'm Bethany!
+  + + Ask how they are doing today
+    I'm doing just fine. 
+
