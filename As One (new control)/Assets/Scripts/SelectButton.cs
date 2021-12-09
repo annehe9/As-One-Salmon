72cf26471902b5e62b5827d30e7d2f8f4926c892
Ameer-J <78869846+Ameer-J@users.noreply.github.com>
2021-12-09 15:48:43 -0500

    more button stuff

--- As One (new control)/Assets/Scripts/SelectButton.cs
+++ As One (new control)/Assets/Scripts/SelectButton.cs
@@ -1,29 +1,38 @@
-using System.Collections;
+  using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SelectButton : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public Sprite defaultSprite;
     public Sprite selectedSprite;
+    public string sceneToLoad;
+    private bool pressed = false;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-        spriteRenderer.sprite = defaultSprite;
-    }
+    void Start(){}
 
     // Update is called once per frame
     void Update()
     {
         
+        if (Input.GetKeyDown("return"))
+        {
+            pressed = true;
+        }
+        else {
+            pressed = false;
+        }
     }
 
     void OnTriggerStay(Collider target) {
         if(target.tag == "Player") {
             spriteRenderer.sprite = selectedSprite;
+        }
+        if (pressed) {
+            SceneManager.LoadScene(sceneToLoad);
         }
     }
 
