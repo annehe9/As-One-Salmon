5c9f3a2b267df268e21bc06f8428b431b9e3ba3f
annehe9 <annehe9@gmail.com>
2021-12-09 15:16:00 -0500

    select button now loads scene

--- As One (new control)/Assets/Scripts/SelectButton.cs
+++ As One (new control)/Assets/Scripts/SelectButton.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SelectButton : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public Sprite defaultSprite;
     public Sprite selectedSprite;
+    public string sceneToLoad;
+    private bool pressed = false;
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +21,21 @@ public class SelectButton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("return"))
+        {
+            pressed = true;
+        }
+        else {
+            pressed = false;
+        }
     }
 
     void OnTriggerStay(Collider target) {
         if(target.tag == "Player") {
             spriteRenderer.sprite = selectedSprite;
+        }
+        if (pressed) {
+            SceneManager.LoadScene(sceneToLoad);
         }
     }
 
